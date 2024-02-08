74e08da41b075cd853b323916aca970c690594b1
GRMSI <grisha.239@gmail.com>
2024-02-08 11:55:26 +0300

    Add blacklist

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import telebot
-from telebot import types
 import os
-from dotenv import load_dotenv
 import requests
+import json
+from telebot import types
+from dotenv import load_dotenv
+from urllib.request import urlopen
 
 load_dotenv()
 token = os.getenv("TOKEN")
@@ -25,21 +27,25 @@ def monitor_chat(message):
 
 @bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
-    username = str(callback.from_user.first_name)
-    bot.edit_message_reply_markup(callback.message.chat.id, callback.message.id, reply_markup=None)
-    if callback.data == 'approve':
-        bot.edit_message_text(chat_id=callback.message.chat.id,
-                              message_id=callback.message.id,
-                              text=callback.message.text + "\n\n✅ The resolution was approved")
-        bot.send_message(callback.message.chat.id,
-                         "✅ The resolution was approved")
-    if callback.data == 'reject':
-        send_json(callback.message, False)
-        bot.edit_message_text(chat_id=callback.message.chat.id,
-                              message_id=callback.message.id,
-                              text=f"{callback.message.text}\n\n⛔️ Please correct the resolution in accordance with the partner's request")
-        bot.send_message(callback.message.chat.id,
-                         "⛔️ Please correct the resolution in accordance with the partner's request")
+    userid = callback.from_user.id
+    response_url = (os.getenv("RESPONSE_URL") + str(userid))
+    response = urlopen(response_url)
+    data_json = json.loads(response.read())
+    if not data_json["IsInBlacklist"]:
+        bot.edit_message_reply_markup(callback.message.chat.id, callback.message.id, reply_markup=None)
+        if callback.data == 'approve':
+            bot.edit_message_text(chat_id=callback.message.chat.id,
+                                  message_id=callback.message.id,
+                                  text=callback.message.text + "\n\n✅ The resolution was approved")
+            bot.send_message(callback.message.chat.id,
+                             "✅ The resolution was approved")
+        if callback.data == 'reject':
+            send_json(callback.message, False)
+            bot.edit_message_text(chat_id=callback.message.chat.id,
+                                  message_id=callback.message.id,
+                                  text=f"{callback.message.text}\n\n⛔️ Please correct the resolution in accordance with the partner's request")
+            bot.send_message(callback.message.chat.id,
+                             "⛔️ Please correct the resolution in accordance with the partner's request")
 
 
 def send_json(message, status):
